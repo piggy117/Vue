8e863bb80c718645fe449192171ad5d58161f531
CrazyCrazyCrazy <ll2543867688@163.com>
2017-12-15 11:09:01 +0800

    01

--- 01/03，变量的结构赋值.js
+++
@@ -1,9 +0,0 @@
-const person = {
-    name:'zs',
-    age:11,
-    gender:'女'
-}
-// 变量的结构赋值
-let {name :name123,age,gender} = person 
-name123 = '哇哈哈'
-console.log(name123)
--- 01/04，startsWith和endsWith.js
+++
@@ -1,4 +0,0 @@
-const str = '11haha!a'
-console.log(str.indexOf('haha') === 2)
-console.log(str.startsWith('11'))
-console.log(str.endsWith('!'))
